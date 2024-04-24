46ccd991d637525bbabdf79fdcc5a6efeef8cf5b
DaedraDante <stefanoctavian12345@gmail.com>
2024-04-24 08:25:03 +0300

    version 1.5
    Added  a very non properly functional upgrade button ))) , right now it does work multiplicatively as intended but with the added bonus of somehow doubling ,and then some ,the current number of dubloons , I need to make it so that it does NOT do that , and substract from the player's dubloons and also make it useable only once , or rather add a new function on it when the first function is being executed , so yeah

--- clicker.js
+++ clicker.js
@@ -2,14 +2,23 @@
 //variable assignation
 const mainImg = document.getElementById("main-img");
 const playerMoneyText = document.getElementById("player-money-text");
+const btnUpgrade1 = document.getElementById("btn-upgrade1");
 //const playerMoneyText = document.getElementById("player-money-text");
 let playerMoney = 0;
+let multiplier = 1;
 // Add click event listener to the image
 mainImg.addEventListener("click", () => {
     // Toggle the "smallify" class
     mainImg.classList.toggle("smallify");
-    playerMoneyText.textContent = playerMoney++
+    playerMoneyText.textContent = playerMoney++ * multiplier;
 });
+btnUpgrade1.addEventListener("click", () => {
+    if(playerMoney >= 25) {
+        multiplier = multiplier + 1
+    }else {
+        alert("not enough money")
+    }
+})
 
 // Function to remove the "smallify" class after the transition ends
 function removeSmallifyClass() {
