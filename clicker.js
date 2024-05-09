8ccc1bcb1e1b568ed133f445f9ccd521ad933731
DaedraDante <stefanoctavian12345@gmail.com>
2024-05-09 12:30:14 +0300

    Version 1.8
    Added the second upgrade button functionability si m,am decis sa tin jocul asta in felul cum e , adica sa nu ii schimb tematica , now I need to make a proper list of integrations and then work on implementing them one by one , step by step :smile

--- clicker.js
+++ clicker.js
@@ -3,6 +3,8 @@
 const mainImg = document.getElementById("main-img");
 const playerMoneyText = document.getElementById("player-money-text");
 const btnUpgrade1 = document.getElementById("btn-upgrade1");
+const btnUpgrade2 = document.getElementById("btn-upgrade2");
+
 let multiplier = 1;
 let playerMoney = 0;
 
@@ -13,19 +15,18 @@ mainImg.addEventListener("click", () => {
     playerMoneyText.textContent = playerMoney;
 });
     
-    // Defining the upgrade costs
+    // Defining the upgrade costs for btnUpgrade1
  const upgradeCosts = [];
  const costOriginal = 25;
- const costAddedPercentage = 35;
+ const costAddedPercentage = 45;
 for(let i = 0; i < 10; i++) {
-     const newCost = Math.floor(costOriginal * Math.pow(1 + costAddedPercentage / 100, i))
+     const newCost = Math.floor(costOriginal * Math.pow(1 + costAddedPercentage / 100, i));
     upgradeCosts.push(newCost);
-}
-
+};
 
     // Upgrade logic for btnUpgrade1
  let upgradeIndex = 0;
- btnUpgrade1.textContent = `Concert no remorse: ${costOriginal} dubloons`
+ btnUpgrade1.textContent = `First Upgrade: ${costOriginal} dubloons`
  btnUpgrade1.addEventListener("click", () => {
  
     if(playerMoney >= upgradeCosts[upgradeIndex]) {
@@ -34,7 +35,7 @@ for(let i = 0; i < 10; i++) {
         playerMoneyText.textContent = playerMoney;
         upgradeIndex += 1;
         if(upgradeIndex < upgradeCosts.length) {
-            btnUpgrade1.textContent = `Concert no remorse: ${upgradeCosts[upgradeIndex]} dubloons`
+            btnUpgrade1.textContent = `First Upgrade: ${upgradeCosts[upgradeIndex]} dubloons`
         }else {
             btnUpgrade1.textContent = "Upgraded to max";
             btnUpgrade1.disabled = true;
@@ -44,6 +45,35 @@ for(let i = 0; i < 10; i++) {
     }
 });
 
+    // Defining the upgrade costs for btnUpgrade2
+const upgradeCosts2 = [];
+const costOriginal2 = 1500;
+const costAddedPercentage2 = 45;
+for(let i = 0; i < 10 ; i++) {
+    const newCost2 = Math.floor(costOriginal2 * Math.pow(1 + costAddedPercentage2 / 100 ,i));
+    upgradeCosts2.push(newCost2); 
+};
+
+    // Upgrade logic for btnUpgrade2
+let upgradeIndex2 = 0;
+btnUpgrade2.textContent = `Second Upgrade: ${costOriginal2} dubloons`
+btnUpgrade2.addEventListener("click",() => {
+
+    if(playerMoney >= upgradeCosts2[upgradeIndex2]) {
+        multiplier += 3;
+        playerMoney -= upgradeCosts2[upgradeIndex2];
+        playerMoneyText.textContent = playerMoney;
+        upgradeIndex2 += 1;
+        if(upgradeIndex2 < upgradeCosts2.length) {
+            btnUpgrade2.textContent = `Second Upgrade : ${upgradeCosts2[upgradeIndex2]} dubloons`
+        }else {
+            btnUpgrade2.textContent = "Upgraded to max";
+            btnUpgrade2.disabled = true;
+        };
+    }else {
+        alert("not enough money");
+    };
+});
 // Function to remove the "smallify" class after the transition ends
 function removeSmallifyClass() {
     mainImg.classList.remove("smallify");
